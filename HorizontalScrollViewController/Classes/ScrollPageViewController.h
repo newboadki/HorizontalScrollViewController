01158006418a2d97e8d97d7890172dc953d3079d
Borja Arias <borja.arias@unboxedconsulting.com>
2011-11-08 11:47:50 +0000

    Changes on ScrollPageViewController to fix crash

---
+++ HorizontalScrollViewController/Classes/ScrollPageViewController.h
@@ -0,0 +1,17 @@
+//
+//  ScrollPageViewController.h
+//  PhotoScroller
+//
+//  Created by Borja Arias Drake on 06/07/2011.
+//  Copyright 2011 Unboxed Consulting. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PageController.h"
+
+@interface ScrollPageViewController : PageController
+{
+
+}
+
+@end
